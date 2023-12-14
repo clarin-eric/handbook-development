0dea144fa9507df0771ec8d138c9d6bbe4526e26
Twan Goosen <twan@clarin.eu>
2023-12-14 17:06:52 +0200

    scripts + docker image for markdown linting

---
+++ lint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -euo pipefail
+SCRIPT_DIR="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
+
+echo "Linting README" \
+	&& bash "${SCRIPT_DIR}/mdl.sh" 'README.md' \
+	&& echo "Linting docs" \
+	&& (cd "${SCRIPT_DIR}/docs" && bash "${SCRIPT_DIR}/mdl.sh" .)
---
+++ mdl.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -euo pipefail
+SCRIPT_DIR="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
+
+echo "Building mdl image"
+docker build --quiet --tag 'mdl:latest' "${SCRIPT_DIR}/docker/mdl"
+
+docker run --rm --name markdown_lint \
+	-v "$(pwd):$(pwd)" -w "$(pwd)" \
+	'mdl:latest' "$@"
