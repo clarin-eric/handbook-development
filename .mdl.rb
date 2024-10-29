27ea2ce2d3e95b3d4fd3e57480b29fd4f18579ca
André Moreira <amoreira.mail@gmail.com>
2024-10-29 17:34:47 +0100

    Expand allowed HTML tags

--- .mdl.rb
+++ .mdl.rb
@@ -3,4 +3,4 @@ rule "MD026", :punctuation => '.,;:!'
 
 # Allow in-line HTML
 #exclude_rule 'MD033'
-rule 'MD033', :allowed_elements => 'link, if, todo'
+rule 'MD033', :allowed_elements => 'link, if, todo, table, thead, tr, th, td, tbody, p, a, strong, br'
